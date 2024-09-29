9f83fe02a96f790f21bed89dfd9d3ffa6fa344f2
Musthak Ahmad Shaik <mshaikh2@murraystate.edu>
2024-09-28 21:47:01 -0500

    list to dict menu items

--- data.py
+++ data.py
@@ -1,26 +1,26 @@
 #use this list of menu items
-menu_items = [
-'D1 ​​SODA 3',
-'D2 ​​LEMONADE 3',
-'D3 ​​TEA 2',
-'D4 WATER 0',
-'A1 ​​POTATO_CAKES 7',
-'A2 ​​SPINACH_DIP 5',
-'A3 ​​OYSTERS 13',
-'A4 ​​CHEESE_FRIES 4',
-'A5 ​​ONION_RINGS 7',
-'S1 ​​COBB 14',
-'S2 ​​CAESAR 13',
-'S3 ​​GREEK 12',
-'E1 ​​BURGER 16',
-'E2 ​​PASTA 15',
-'E3 ​​GNOCCHI​   14',
-'E4 ​​GRILLED_STEAK_SANDWICH 17',
-'T1 ​​CARAMEL_CHEESECAKE 13',
-'T2 ​​APPLE_COBBLER 12',
-'T3 ​​BROWNIE_SUNDAE 9',
-'T4 ​​FLAN 8'
-]
+menu_items = {
+'D1': ['SODA', 3],
+'D2': ['LEMONADE', 3],
+'D3': ['TEA' ,2],
+'D4': ['WATER', 0],
+'A1': ['POTATO_CAKES', 7, 33],
+'A2': ['SPINACH_DIP', 5, 43],
+'A3': ['OYSTERS', 13, 28],
+'A4': ['CHEESE_FRIES', 4, 30],
+'A5': ['ONION_RINGS', 7, 27],
+'S1': ['COBB', 14, 35],
+'S2': ['CAESAR', 13, 36],
+'S3': ['GREEK', 12, 48],
+'E1': ['BURGER', 16, 40],
+'E2': ['PASTA', 15, 32],
+'E3': ['GNOCCHI',14, 31],
+'E4': ['GRILLED_STEAK_SANDWICH',17, 26],
+'T1': ['CARAMEL_CHEESECAKE',13, 48],
+'T2': ['APPLE_COBBLER',12, 49],
+'T3': ['BROWNIE_SUNDAE',9, 49],
+'T4': ['FLAN', 8, 39]
+}
 drink_items = ['D1', 'D2',  'D3', 'D4']
 appetizer_items = ['A1', 'A2',  'A3', 'A4', 'A5']
 salad_items = ['S1', 'S2', 'S3']
